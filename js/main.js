59df4e0fdb371671f721764f308a2ae0fdb2147c
EkaterinaTadueva <etadueva@bk.ru>
2022-06-14 22:34:26 +0300

    Дописала функцию для создания рондомных фото и комментариев

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-const NUMBERS_ID_COMMENTS = Array.from({ length: 100 }, (v, i) =>  i + 1);
-
-const NUMBERS_ID_PHOTO = Array.from({ length: 25 }, (v, i) =>  i + 1);
-
-const NUMBERS_NAME_PHOTO = Array.from({ length: 25 }, (v, i) =>  i + 1);
-
 const DEFINITION = [
   'Красиво!',
   'Странное искуство',
@@ -43,6 +37,7 @@ const NAMES_AUTHORS = [
 ];
 
 const SIMILAR_DESCRIPTIONS_PHOTOS_COUNT = 25;
+const NUMBERS_OF_COMMENTS = 4;
 
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
@@ -55,32 +50,45 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const checkStringLength = (string, maxLength) => string.length <= maxLength;
-
-checkStringLength('Комментарий', 140);
-
 const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
 
-
-const createListComments = () => ({
-  id: getRandomArrayElement(NUMBERS_ID_COMMENTS),
+const createListComments = (id) => ({
+  id,
   avatar: `img/avatar-${  getRandomInt(0, 6)  }.svg`,
   message: getRandomArrayElement(TEXT_COMMENTS),
   name: getRandomArrayElement(NAMES_AUTHORS),
 });
 
-const NUMBERS_OF_COMMENTS = getRandomInt(1, 5);
-const similarComments = Array.from({length: NUMBERS_OF_COMMENTS}, createListComments);
+const makeComments = (count) => {
+  const similarComments = [];
+  for (let i = 1; i <= count; i++) {
+    similarComments.push(createListComments(i));
+  }
 
-const getDescriptionPhotos = () => ({
-  id: getRandomArrayElement(NUMBERS_ID_PHOTO),
-  url: `photos/${  getRandomInt(NUMBERS_NAME_PHOTO)  }.jpg`,
+  return similarComments;
+};
+
+const getDescriptionPhotos = (id) => ({
+  id,
+  url: `photos/${  id  }.jpg`,
   description: getRandomArrayElement(DEFINITION),
   likes: getRandomInt(15, 200),
-  comments: similarComments
+  comments: makeComments(NUMBERS_OF_COMMENTS)
 });
 
-const similarDescriptionPhotos = Array.from({length: SIMILAR_DESCRIPTIONS_PHOTOS_COUNT}, getDescriptionPhotos);
+const makePhotos = (count) => {
+  const similarPhotos = [];
+  for (let i = 1; i <= count; i++) {
+    similarPhotos.push(getDescriptionPhotos(i));
+  }
 
-// eslint-disable-next-line no-console
-console.log(similarDescriptionPhotos);
+  return similarPhotos;
+};
+
+makePhotos(SIMILAR_DESCRIPTIONS_PHOTOS_COUNT);
+
+//Вспомогательная функция, которая еще не задействована:
+
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+
+checkStringLength('Комментарий', 140);
