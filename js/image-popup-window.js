41e1d3aad91200f3be803ec4e48e20bdea044eb0
EkaterinaTadueva <etadueva@bk.ru>
2022-07-02 15:16:23 +0300

    Правки дз: Отрисуй меня полностью(часть 2)

--- js/image-popup-window.js
+++ js/image-popup-window.js
@@ -5,42 +5,42 @@ const MIN_NUMBER_COMMENTS = 5;
 const MAX_NUMBER_COMMENTS = 10;
 const NUMBERS_OF_COMMENTS = getRandomInt(MIN_NUMBER_COMMENTS, MAX_NUMBER_COMMENTS);
 const NUMBERS_OF_READY_COMMENTS = 2;
-const widthAvatar = 35;
-const heightAvatar = 35;
+const AVATAR_WIDTH = 35;
+const AVATAR_HEIGHT = 35;
 
 const popupImage = document.querySelector('.big-picture');
 const imageBlock = popupImage.querySelector('.big-picture__img');
 const image = imageBlock.querySelector('img');
-const arrayComments = makeComments(NUMBERS_OF_COMMENTS);
+const commentsData = makeComments(NUMBERS_OF_COMMENTS);
 const commentList = popupImage.querySelector('.social__comments');
 const commentTemplate = commentList.querySelector('.social__comment');
-const buttonClosePopup = document.querySelector('.big-picture__cancel');
+const closePopupButton = document.querySelector('.big-picture__cancel');
 const body = document.querySelector('body');
 
-const renderingBigImage = (photoData) => {
+const renderBigImage = (photoData) => {
   popupImage.classList.remove('hidden');
   image.setAttribute('src', photoData.url);
   popupImage.querySelector('.likes-count').textContent = photoData.likes;
   popupImage.querySelector('.comments-count').textContent = NUMBERS_OF_COMMENTS + NUMBERS_OF_READY_COMMENTS;
   popupImage.querySelector('.social__caption').textContent = photoData.description;
-  arrayComments.forEach(({avatar, name, message}) => {
-    const commentItem = commentTemplate.cloneNode(true);
-    const avatarItem = commentItem.querySelector('.social__picture');
+  commentsData.forEach(({avatar, name, message}) => {
+    const comment = commentTemplate.cloneNode(true);
+    const avatarItem = comment.querySelector('.social__picture');
     avatarItem.setAttribute('src', avatar);
     avatarItem.setAttribute('alt', name);
-    avatarItem.setAttribute('width', widthAvatar);
-    avatarItem.setAttribute('height', heightAvatar);
-    commentItem.querySelector('.social__text').textContent = message;
-    commentList.appendChild(commentItem);
+    avatarItem.setAttribute('width', AVATAR_WIDTH);
+    avatarItem.setAttribute('height', AVATAR_HEIGHT);
+    comment.querySelector('.social__text').textContent = message;
+    commentList.appendChild(comment);
   });
-  const commentCounter = document.querySelector('.social__comment-count');
-  commentCounter.classList.add('hidden');
+  const commentCount = document.querySelector('.social__comment-count');
+  commentCount.classList.add('hidden');
   const uploadingNewComments = document.querySelector('.comments-loader');
   uploadingNewComments.classList.add('hidden');
   body.classList.add('modal-open');
 };
 
-buttonClosePopup.addEventListener('click', () => {
+closePopupButton.addEventListener('click', () => {
   popupImage.classList.add('hidden');
   body.classList.remove('modal-open');
 });
@@ -52,4 +52,4 @@ document.addEventListener('keydown', (e) => {
   }
 });
 
-export { renderingBigImage };
+export { renderBigImage };
