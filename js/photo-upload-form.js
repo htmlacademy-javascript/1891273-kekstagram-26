7114a72799471eca0273ce10f25ef087a489e9c6
EkaterinaTadueva <etadueva@bk.ru>
2022-07-18 14:50:40 +0300

    Сделала зум загружаемого изображения

--- js/photo-upload-form.js
+++ js/photo-upload-form.js
@@ -1,6 +1,9 @@
 const MAX_NUMBER_HASHTAGS = 5;
 const MAX_LENGTH_COMMENT = 140;
 const MAX_LENGTH_HASHTAG = 20;
+const SCALING_STEP = 25;
+const MIN_SCALE = 25;
+const MAX_SCALE = 100;
 
 const imageUploadForm = document.querySelector('#upload-file');
 const imageEditingForm = document.querySelector('.img-upload__overlay');
@@ -9,6 +12,49 @@ const commentInput = document.querySelector('.text__description');
 const cancelButton = document.querySelector('.img-upload__cancel');
 const form = document.querySelector('.img-upload__form');
 const body = document.querySelector('body');
+const zoomControl = document.querySelector('.scale__control--value');
+const zoomOutButton = document.querySelector('.scale__control--smaller');
+const zoomInButton = document.querySelector('.scale__control--bigger');
+const uploadedImageBlock = document.querySelector('.img-upload__preview');
+const uploadedImage = uploadedImageBlock.querySelector('img');
+
+const changeImageSize = () => {
+  if (zoomControl.value === '25%') {
+    uploadedImage.style = 'transform: scale(0.25)';
+  }
+  if (zoomControl.value === '50%') {
+    uploadedImage.style = 'transform: scale(0.5)';
+  }
+  if (zoomControl.value === '75%') {
+    uploadedImage.style = 'transform: scale(0.75)';
+  }
+  if (zoomControl.value === '100%') {
+    uploadedImage.style = 'transform: scale(1)';
+  }
+};
+
+const zoomOut = () => {
+  let zoomControlValue = Math.floor(zoomControl.value.replace('%', ''));
+  if (zoomControlValue > MIN_SCALE) {
+    zoomControlValue = zoomControlValue - SCALING_STEP;
+  } else {
+    zoomControlValue = MIN_SCALE;
+  }
+  zoomControl.value = `${ zoomControlValue }%`;
+  changeImageSize();
+};
+
+
+const zoomIn = () => {
+  let zoomControlValue = Math.floor(zoomControl.value.replace('%', ''));
+  if (zoomControlValue < MAX_SCALE - SCALING_STEP) {
+    zoomControlValue = zoomControlValue + SCALING_STEP;
+  } else {
+    zoomControlValue = MAX_SCALE;
+  }
+  zoomControl.value = `${ zoomControlValue }%`;
+  changeImageSize();
+};
 
 const preparedHashtags = (value) => value.trim().toLowerCase().split(' ');
 
@@ -79,6 +125,8 @@ const mistakes = [
 ];
 
 const uploadImage = (pristine) => {
+  zoomOutButton.addEventListener('click', zoomOut);
+  zoomInButton.addEventListener('click', zoomIn);
   imageUploadForm.addEventListener('change', openDownloadWindow);
   mistakes.forEach( (obj) => {
     pristine.addValidator(hashtagsInput, obj.check, obj.comment);
