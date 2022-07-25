3bd802c0e21aaf9d1096d07c7b964d23c9f01c15
EkaterinaTadueva <etadueva@bk.ru>
2022-07-26 02:52:22 +0300

    Перламутровые пуговицы в процессе

--- js/rendering-thumbnails.js
+++ js/rendering-thumbnails.js
@@ -2,8 +2,39 @@ import { renderBigImage } from './image-popup-window.js';
 
 const picturesList = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const imageFilters = document.querySelector('.img-filters');
+const filterButtons = document.querySelectorAll('.img-filters__button');
 
 const similarListFragment = document.createDocumentFragment();
+
+const compareLengthComments = (arr) => {
+  const commentsArray = arr.comments;
+  const commentsLength = commentsArray.length;
+  return commentsLength;
+};
+
+const sortingPicture = (first, next) => {
+  const rankA = compareLengthComments(first);
+  const rankB = compareLengthComments(next);
+
+  return rankB - rankA;
+};
+
+const filterSettings = {
+  default: (arr) => arr,
+  random: 'shuffle',
+  popular: (arr) => arr.sort(sortingPicture),
+};
+
+const openFilter = () => {
+  imageFilters.classList.remove('img-filters--inactive');
+};
+
+const makeButtonsInactive = () => {
+  filterButtons.forEach((button) => {
+    button.classList.remove('img-filters__button--active');
+  });
+};
 
 const renderPhotos = (picture) => {
   picture.forEach(({url, likes, comments, description}) => {
@@ -15,7 +46,22 @@ const renderPhotos = (picture) => {
     similarListFragment.appendChild(pictureElement);
     pictureElement.addEventListener('click', () => renderBigImage({url, likes, comments, description}));
   });
-  return picturesList.appendChild(similarListFragment);
+  picturesList.appendChild(similarListFragment);
+  openFilter();
 };
 
-export { renderPhotos };
+const selectFilter = (images) => {
+  filterButtons.forEach((button) => {
+    button.addEventListener('click', (evt) => {
+      makeButtonsInactive();
+      const filterButton = evt.target;
+      filterButton.classList.add('img-filters__button--active');
+      const viewType = filterButton.dataset.sorting;
+      const getPictures = filterSettings[viewType];
+      const pictures = getPictures(images);
+      renderPhotos(pictures);
+    });
+  });
+};
+
+export { renderPhotos, openFilter, selectFilter };
